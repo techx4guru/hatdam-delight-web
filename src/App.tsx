1a657b4cf5dea9052158f7fb953b0a733bcea7b9
techx4guru <techx4.guru@gmail.com>
2025-09-18 17:15:45 +0100

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Products from "./pages/Products";
-import Order from "./pages/Order";
+import Order from "./pages/Orderbooking";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
